e77d663f040c1240bab41f0cf6076e020aef1703
andrewyohanes123 <andrew.yohanes123@gmail.com>
2021-10-09 20:39:40 +0800

    +module 3 (not done yet)

--- src/data/moduleName.tsx
+++ src/data/moduleName.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import Module1 from "../pages/Module1";
 import Module2 from "../pages/Module2";
+import Module3 from "../pages/Module3";
 
 export interface ModulePageInterface {
   name: string;
@@ -16,7 +17,12 @@ export const pageModules: ModulePageInterface[] = [
   },
   {
     name: 'Modul 2',
-    description: 'RANGKAIAN SERI DENGAN STOP KONTAK ',
+    description: 'RANGKAIAN SERI DENGAN STOP KONTAK',
     component: <Module2 />
+  },
+  {
+    name: 'Modul 3',
+    description: 'RANGKAIAN DUA ARAH DENGAN KONTAK UTAMA',
+    component: <Module3 />
   },
 ]
